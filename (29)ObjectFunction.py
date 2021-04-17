e95703e16b885ad9b83c5084ec8f543be7cb42f2
fekrysaharudin <fekrysaharudin@users.noreply.github.com>
2021-04-17 15:01:15 +0800

    Beginner

---
+++ (29)ObjectFunction.py
@@ -0,0 +1,6 @@
+from Student import Student
+
+student1 = Student("Oscar", "Accounting", 3.1, True)
+student2 = Student("Phyllis", "Business", 3.8, False)
+
+print(student1.on_honor_roll())
---
+++ ChineseChef.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+
+class ChineseChef(Chef):
+
+    def make_special_dish(self):                #Override the function
+        print("The chef makes orange chicken")
+
+    def make_friend_rice(self):
+        print("The chef makes friend rice")
